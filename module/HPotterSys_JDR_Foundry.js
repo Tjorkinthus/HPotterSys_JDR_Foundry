daf0b40045ac6a9db0ea55144c7342b1a8aa825c
Tjorkinthus <56727760+Tjorkinthus@users.noreply.github.com>
2021-09-02 15:57:58 +0200

    Append

--- module/HPotterSys_JDR_Foundry.js
+++ module/HPotterSys_JDR_Foundry.js
@@ -1,12 +1,14 @@
 import HPotterSys_JDR_FoundryItemSheet from "./sheets/HPotterSys_JDR_FoundryItemSheet.js";
 import HPotterSys_JDR_FoundryActorSheet from "./sheets/HPotterSys_JDR_FoundryActorSheet.js";
 
-Hooks.once("init", () => {
+Hooks.once("init", function() {
     console.log("HPotterSys_JDR_Foundry | Initialisation du système HP");
-
+    
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("HPotterSys_JDR_Foundry", HPotterSys_JDR_FoundryItemSheet, {makeDefault: true});
 
     Actors.unregisterSheet("core", ActorSheet);
     Actors.registerSheet("HPotterSys_JDR_Foundry", HPotterSys_JDR_FoundryActorSheet, {makeDefault: true});
-})
+
+   
+});
